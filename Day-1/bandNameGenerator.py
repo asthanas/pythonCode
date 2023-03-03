4c9a974d32b119a3332c93c7e7a5ff9b14201dfd
Saurabh Asthana <xxx.@gmail.com>
2023-03-03 22:28:13 +0530

    Revert "band name generator"
    This reverts commit da0ffbfa75efe99b629e026d0fd046228cd49936.

--- Day-1/bandNameGenerator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the Band Name Generator.\n")
-city = input("Whats the name of city you grew up in ?\n")
-pet = input("Whats your pet's name ? \n")
-print("Your band name is " + city + " " + pet)
